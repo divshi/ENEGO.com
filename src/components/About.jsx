488a1731037b76fd21846e09384272ddab86a2d7
Divya Shrivastava <shridivya893@gmail.com>
2024-11-14 02:34:27 +0530

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,15 +9,10 @@ const AboutSection = () => {
         
         {/* Image Section with Shadow */}
         <div className="relative w-2/5 pr-8">
-          {/* Shadow Box */}
-          <div
-            className="absolute top-3 left-3 w-full h-full bg-blue-900 opacity-90 rounded-lg"
-            style={{ zIndex: -1 }}
-          ></div>
           <img
             src={About}
             alt="About Us"
-            className="rounded-lg shadow-lg w-full"
+            className="rounded-lg shadow-3d w-full"
           />
         </div>
 
@@ -26,9 +21,9 @@ const AboutSection = () => {
           <h2 className="text-5xl font-bold text-blue-900">Who we Are?</h2>
           <hr className="w-[250px] border-t-2 border-black mb-2" />
           <p className="text-lg text-gray-700">
-          We help foster collaboration, gather resources, and facilitate strategic partnerships to build a thriving community of entrepreneurs driving impactful change.
-          <br /><br />Enego aims to simplify the journey for startups and is committed to offering tailored services in various verticals that help promote a healthy start-up. We understand the challenges faced by emerging businesses and strive to be the driving force behind their success. 
-          <br />
+            We help foster collaboration, gather resources, and facilitate strategic partnerships to build a thriving community of entrepreneurs driving impactful change.
+            <br /><br />Enego aims to simplify the journey for startups and is committed to offering tailored services in various verticals that help promote a healthy start-up. We understand the challenges faced by emerging businesses and strive to be the driving force behind their success. 
+            <br />
           </p>
           <button className="mt-[50px] px-6 py-2 bg-blue-900 text-white rounded-lg shadow-lg hover:bg-white hover:text-blue-900">
             Know more <FaArrowRight className="inline ml-2" />
@@ -36,6 +31,16 @@ const AboutSection = () => {
         </div>
         
       </div>
+
+      <style jsx>{`
+          .shadow-3d {
+            box-shadow: 
+              4px 4px 6px rgba(15, 23, 42, 0.5),   /* Soft shadow with blue-900 */
+              8px 8px 20px rgba(15, 23, 42, 0.6),  /* Medium shadow with blue-900 */
+              12px 12px 40px rgba(15, 23, 42, 0.7); /* Deeper shadow with blue-900 */
+          }
+        `}</style>
+
     </section>
   );
 };
